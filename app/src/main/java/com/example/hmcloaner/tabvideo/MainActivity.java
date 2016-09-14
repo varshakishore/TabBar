df50e6b90eaf315c4a2016d1e7f1c136d5048f34
HMCLoaner <HMCLoaner@WL-198-55.WPA.Claremont.Edu>
2016-09-13 22:47:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/hmcloaner/tabvideo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.hmcloaner.tabvideo;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar)findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragments(new HomeFragment(), "Home");
+        viewPagerAdapter.addFragments(new TopFreeFragment(), "Top Free");
+        viewPagerAdapter.addFragments(new TopPaidFragment(), "Top Paid");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
